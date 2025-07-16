59591f863e25442520833d1193527a2a9a51ba8c
prudhvi ganesh <prudhviredrouth143@gmail.com>
2025-07-16 15:20:03 +0530

    model winner

--- src/exception.py
+++ src/exception.py
@@ -1,25 +1,43 @@
-import sys
-from src.logger import logging
+## A child went to toy shop with his father,to buy a topy 
+## child = customException
+## Toy = Error message
+## Father = main Exception
+## cctv in shop = sys
 
-## With this below function we are deciding how our message should look like
-def error_message_details(error,error_detail:sys):
+
+import sys ## CCtv of toy shop
+
+
+def error_message_detail(error_msg,error_detail:sys):
+
+    ''' Crafting our own custom message'''
     _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename     ## checking in whcih file the error is getting
+    file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
-
+        file_name,exc_tb.tb_lineno,str(error_msg))
+    
     return error_message
 
 
 
-class CustomException(Exception):
+
+
+class CustomException(Exception):   ## customException is child
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message_details(error_message,error_detail=error_detail)
+        self.error_message = error_message_detail(error_message,error_detail)
 
     def __str__(self):
         return self.error_message
-    
 
+if __name__ == '__main__':
 
+    try:
+        a = 10
+        b = 0
+        print(a/b)
+    except Exception as e:
+        ce =  CustomException(e,sys)
+        print(ce)
+        
     
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
-import os 
+import os
 import sys
-import numpy as np
-import pandas as pd 
-import dill
-from src.exception import CustomException
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
 
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
@@ -14,35 +16,30 @@ def save_object(file_path, obj):
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj,file_obj)
-            
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
-    try:
-
-        report = {}
-
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-
-            model.fit(x_train,y_train) # Train model
-
-            y_train_pred = model.predict(x_train)
-
-            y_test_pred = model.predict(x_test)
-
-            train_model_score = r2_score(y_train,y_train_pred)
-
-            test_model_score = r2_score(y_test,y_test_pred)
-
-            report[list(model.keys()[i])] = test_model_score
-
-        return report
 
     except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def save_info(model_name,best_score,file_path):
+    try:
+        with open(file_path, 'w') as file_obj:
+            file_obj.write(f"The winner is {model_name} with best score of {best_score}")
+            file_obj.close()
+    except Exception as e:
         raise CustomException(e,sys)
-        
